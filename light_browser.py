b4265591596fe47671ef8dbeaea82acde5476ed8
Alvkraft <35309997+Alv777@users.noreply.github.com>
2023-08-06 05:35:34 +0200

    Added tabs & more!
    On this update, tabs were added they are located on top of the navigation bar this took me some time at least to learn how to do it correctly, also blocked the navigation bar so you can't drag it arround which was present on the previous version, also added a new style that made buttons bigger and a better UI in general. Also added a way to close the tabs created, and you can drag them to re-order them. In next updates you will be able to pin them.
    
    Also deleted some un-used imports and added a resize fo the window that doesn't work very well but makes a good job on keeping the window usable at the start. Without it, the window is very small.

--- light_browser.py
+++ light_browser.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QMainWindow, QToolBar, QAction, QLineEdit, QApplication
+from PyQt5 import (QtCore, QtWidgets)
+from PyQt5.QtWidgets import (QMainWindow, QPushButton, QLabel, QLineEdit)
+from PyQt5.QtWidgets import (QFormLayout, QSizePolicy, QAction, QToolBar, QApplication)
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 
 
 class MainWindow(QMainWindow):
@@ -12,8 +14,10 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        # navbar
+
+        # Navigation Bar
         navbar = QToolBar()
+        navbar.setMovable(False)  # Lock the toolbar
         self.addToolBar(navbar)
 
         back_btn = QAction('Back', self)
@@ -31,12 +35,35 @@ class MainWindow(QMainWindow):
         home_btn = QAction('Home', self)
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
+        
+        new_tab = QAction('New Tab', self)
+        new_tab.triggered.connect(self.newTabHandler)
+        navbar.addAction(new_tab)
 
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
 
         self.browser.urlChanged.connect(self.update_url)
+        
+        self.resize(800, 600) # Set width and height (Doesn't even work but at least it's a razonable size lol)
+        
+    def newTabHandler(self):
+        tab_widget = self.parentWidget().parentWidget()
+        print(tab_widget)
+        count = tab_widget.count()
+        #print(count + 1) # Total tabs opened
+        win = MainWindow()
+        tab_widget.addTab(win, "New Tab {}".format(count + 1))
+        tab_widget.setTabsClosable(True)
+        tab_widget.tabCloseRequested.connect(self.close_tab)
+        tab_widget.setDocumentMode(True)
+        tab_widget.setMovable(True)
+    
+    def close_tab(self, index):
+        tab_widget = self.parentWidget() 
+        tab_widget = tab_widget.parentWidget()
+        tab_widget.removeTab(index)
 
     def navigate_home(self):
         self.browser.setUrl(QUrl('https://google.com'))
@@ -49,7 +76,14 @@ class MainWindow(QMainWindow):
         self.url_bar.setText(q.toString())
 
 
-app = QApplication(sys.argv)
-QApplication.setApplicationName('Light Browser')
-window = MainWindow()
-app.exec_()
+if (__name__ == "__main__"):
+    app = QtWidgets.QApplication(sys.argv)
+    QApplication.setStyle("fusion")
+    tabs = QtWidgets.QTabWidget()
+    win = MainWindow()
+    tabs.addTab(win, "New Tab 1" )
+    tabs.show()
+    tabs.setTabsClosable(True)
+    tabs.setDocumentMode(True)
+    tabs.setMovable(True)
+    sys.exit (app.exec_())
